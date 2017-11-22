b16a010b612f6925f00d915638f11efc96139c65
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-11-22 14:46:18 +0100

    Config eslint and webpack

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,6 @@
+/* globals window, URL, navigator, fetch */
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+/* eslint no-param-reassign: ["error", { "props": false }] */
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
