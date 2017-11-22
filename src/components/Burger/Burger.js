bb10fa77df0617e5dffb532434e4e2b62520f6c7
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-11-22 17:52:06 +0100

    Pass ingredients dinamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classes from './Burger.css';
 import BurgerIngredient from '../BurgerIngredient/BurgerIngredient';
 
-const Burger = () => (
-  <div className={classes.Burger}>
-    <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="bread-bottom" />
-  </div>
-);
+const Burger = ({ ingredients }) => {
+  const transformedIngredients = Object.keys(ingredients).map(igKey =>
+    [...Array(ingredients[igKey])].map(() => (
+      <BurgerIngredient key={igKey} type={igKey} />
+    ))
+  );
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
+Burger.propTypes = {
+  ingredients: PropTypes.shape({})
+};
 export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import AuxHoc from '../../hoc/AuxHoc';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
+    const { ingredients } = this.state;
     return (
       <AuxHoc>
-        <Burger />
+        <Burger ingredients={ingredients} />
         <div>Build</div>
       </AuxHoc>
     );
