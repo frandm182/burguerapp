6715d85819be094be71eb64639e66369e4a84949
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-11-24 12:44:40 +0100

    Add SIdeDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import AuxHoc from '../../hoc/AuxHoc';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 import classes from './Layout.css';
 
 const layout = ({ children }) => (
   <AuxHoc>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{children}</main>
   </AuxHoc>
 );
